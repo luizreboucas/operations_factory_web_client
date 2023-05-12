00807ff796c48ca4695e710fb06302b56285e03d
luizreboucas <primoneto@gmail.com>
2023-05-12 11:19:56 -0300

    implementação post

--- src/app/showpage/page.tsx
+++ src/app/showpage/page.tsx
@@ -6,32 +6,41 @@ import axios from 'axios'
 interface CategoriasProps {
   nome: string,
   __v: number,
-  _id: string
-}
-interface OperacoesProps{
   _id: string,
-  tipo: string
-  categoria: string
-  valor: number,
-  __v: number
+  subcategorias: string[]
+}
+interface NovaOperacaoProps{
+  categoria: string,
+  subcategoria: string,
+  descricao: string,
+  valor: number
 }
 
 
 const Show = () => {
-  const [categorias,setCategorias] = useState<CategoriasProps[]>([])
-  const [operacoes, setOperacoes] = useState<OperacoesProps[]>([])
+  
+  const [categorias,setCategorias] = useState<CategoriasProps[] | undefined>([])
+  const [subcategorias, setSubcategorias] = useState<string[] | undefined>()
+  const [novaOperacao, setNovaOperacao] = useState<NovaOperacaoProps>({
+    categoria: '',
+    subcategoria: '',
+    descricao: '',
+    valor: 0
+  })
+  
+  
   const [mounted,setMounted] = useState<boolean>(false)
   useEffect(()=>{
 
     const getCategorias = async()=>{
       try {
-        const cat : CategoriasProps[] = await (await axios.get('http://localhost:3500/categorias')).data
+        const cat : CategoriasProps[] = (await axios.get('http://localhost:3500/categorias')).data
         setCategorias(cat)
         
-
       } catch (error) {
         console.log(error)
       }
+      console.log(categorias)
     }
     getCategorias()
     setMounted(true)
@@ -39,42 +48,62 @@ const Show = () => {
   },[])
   
   const [categoria,setCategoria] = useState<CategoriasProps>()
-  const escolheuCategoria = async(e: any) =>{
-    
+  const escolheuCategoria = (e : React.ChangeEvent<HTMLSelectElement>)=>{
+    const nomeCategoriaEscolhida : string = e.target.value
+    const categoriaEscolhida : (CategoriasProps | undefined) = categorias?.find(item => item.nome == nomeCategoriaEscolhida)
+    setSubcategorias(categoriaEscolhida?.subcategorias)
+    setNovaOperacao({...novaOperacao, categoria: nomeCategoriaEscolhida})
+  }
+
+  const enviarCadastro = async(event:  React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
+    event.preventDefault()
     try {
-      const nomeCategoria = e.target.value
-      const categoriaSelecionada: CategoriasProps = categorias.filter(cat=> cat.nome === nomeCategoria)
-      setCategoria(categoriaSelecionada)
-      console.log(categoriaSelecionada)
-      
-      const operacoesDb : OperacoesProps[] = (await axios.get('http://localhost:3500/operacoes')).data
-     
-     console.log(categoriaSelecionada?._id)
-     
+      setNovaOperacao({...novaOperacao, categoria: "645d2a6037efd604d363318f"})
+      console.log(novaOperacao)
     } catch (error) {
       console.log(error)
     }
-
   }
 
   
   return (
-    <div>
-      <form action="" className='flex flex-col'>
-        <h2>Escolha a Categoria Desejada</h2>
+    <div className='h-full  flex flex-col items-center justify-center bg-blue-200'>
+      <form action="" className=' text-2xl flex-wrap bg-slate-50 p-8 rounded-xl'>
+        
         {mounted
-        ? (<select onChange={(e)=>escolheuCategoria(e)}>
-          {categorias.map((item)=>{
+        ? (<div className='flex flex-col gap-8'>
+          <h2 className='font-bold text-slate-600'>Escolha a Categoria Desejada</h2>
+          <select onChange={(e)=>escolheuCategoria(e)}  className='p-3 rounded-lg'>
+          {categorias?.map((item)=>{
             return <option key={item._id}  >{item.nome}</option>
           })}
-        </select>): ''}
-        <h2>Escolha A Operação </h2>
+        </select></div>): ''}
+        
         {mounted
-        ? (<select onChange={(e)=>escolheuCategoria(e)}>
-          {operacoes.map((item)=>{
-            return <option key={item._id} >{item.tipo}</option>
+        ? (<div className='flex flex-col gap-8 '>
+          <h2 className='mt-8 font-bold text-slate-600'>Escolha A Subcategoria </h2>
+          <select onChange={(event)=> setNovaOperacao({...novaOperacao,subcategoria: event.target.value})}  className='p-3 rounded-lg'>
+          {subcategorias?.map((item)=>{
+            return <option key={item}>{item}</option>
           })}
-        </select>): ''}
+        </select>
+        <input 
+          type='text' 
+          placeholder='descricao' 
+          className='p-3 rounded-lg' 
+          onChange={(event)=> setNovaOperacao({...novaOperacao, descricao: event.target.value})}
+        />
+        <input 
+          type='number' 
+          placeholder='valor'  
+          className='p-3 rounded-lg'
+          onChange={(event)=> setNovaOperacao({...novaOperacao, valor: parseFloat(event.target.value) })}
+        />
+        <button 
+          className='text-slate-50 text-xl font-bold bg-blue-600 p-3 rounded-lg '
+          onClick={(e) => enviarCadastro(e)}>Cadastrar</button>
+        </div>): ''}
+        
       
       </form>
     </div>
